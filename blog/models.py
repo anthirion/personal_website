64d9450e9251fa8f7bfbaf6de5696a35cb26d8b2
antoinethirion <antoine.thirion@wavestone.com>
2026-03-05 12:04:52 +0100

    feat: create models and configure db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+
+class Article(models.Model):
+  STATUS_CHOICES = (
+      ('draft', 'Draft'),
+      ('published', 'Published'),
+  )
+  title = models.CharField(max_length=200, unique=True)
+  slug = models.SlugField(max_length=200, unique=True)
+  category = models.ForeignKey(
+      Category, on_delete=models.SET_NULL, null=True, related_name='articles')
+  content = models.TextField(unique=True)
+  excerpt = models.TextField(max_length=500, unique=True)
+
+  published_at = models.DateTimeField(default=timezone.now)
+  updated_at = models.DateTimeField(auto_now=True)
+  status = models.CharField(
+      max_length=20, choices=STATUS_CHOICES, default='draft')
+
+  # class Meta:
+  #   ordering = ('-published_at',)
+
+  def __str__(self):
+    return self.title
