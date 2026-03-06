28981aee39271e8cf945f1f8e086886b330d81c5
antoinethirion <antoine.thirion@wavestone.com>
2026-03-06 11:09:13 +0100

    fix: corriger le pluriel de Category dans l'interface d'admin

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Category(models.Model):
   name = models.CharField(max_length=50)
 
+  class Meta:
+    verbose_name_plural = 'Categories'
+
   def __str__(self):
     return self.name
 
